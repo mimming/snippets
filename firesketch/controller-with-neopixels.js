b919ade9df596377a617970ff45a7445f67f5cf6
Jenny Tong <mimming@google.com>
2015-10-16 15:23:04 -0700

    added neopixel enabled version

---
+++ firesketch/controller-with-neopixels.js
@@ -0,0 +1,61 @@
+var pixel = require("node-pixel");
+var five = require("johnny-five");
+var Firebase = require("firebase");
+
+var rootRef = new Firebase("https://firesketch.firebaseio-demo.com/");
+var board = new five.Board();
+board.on("ready", function() {
+
+	var strip = new pixel.Strip({
+		data: 6,
+		length: 8,
+		board: this,
+		controller: "FIRMATA",
+	});
+  
+
+  potX = new five.Sensor({
+    pin: "A0",
+    freq: 10
+  });
+  potY = new five.Sensor({
+    pin: "A1",
+    freq: 10
+  });
+
+  var BOARD_SIZE = 60;
+
+  var potXLast = 0;
+  var potYLast = 0;
+
+  var color = Math.floor((Math.random() * 4096)).toString(16);
+  console.log("color: " + color);
+
+	strip.on("ready", function() {
+		strip.color("#" + color);
+	  strip.show();
+	});
+
+  potX.on("data", function() {
+    var cleanX = Math.floor(potX.value * BOARD_SIZE / 1023);
+    if(potXLast != cleanX) {
+      potXLast = cleanX;
+      console.log("new X " + potXLast);
+      writeChange();
+    }
+  });
+
+  potY.on("data", function() {
+    var cleanY = Math.floor(potY.value * BOARD_SIZE / 1023);
+    if(potYLast != cleanY) {
+      potYLast = cleanY;
+      console.log("new Y " + potYLast);
+      writeChange();
+    }
+  });
+
+  function writeChange() {
+    console.log("write: " + potXLast + " x " + potYLast);
+    rootRef.child(potXLast + ":" + potYLast).set(color);
+  }
+});
