0fdda255a7650ca352fd668cf6abf9a703d65940
Jen Tong <mimming@google.com>
2017-10-24 11:57:18 -0700

    Cloud function now pages down to programming community

--- twitch-tracker/functions/index.js
+++ twitch-tracker/functions/index.js
@@ -13,58 +13,80 @@
 //  limitations under the License.
 
 const functions = require('firebase-functions');
-const request = require('request');
 const firebase = require('firebase');
+const rp = require('request-promise');
+const _ = require('lodash');
 
-var config = {
-	apiKey: "AIzaSyBmk5NHsNEri6J1vISirSwsNQvIV3zaC5k",
-	authDomain: "twitch-tracker.firebaseapp.com",
-	databaseURL: "https://twitch-tracker.firebaseio.com",
-	projectId: "twitch-tracker",
-	storageBucket: "twitch-tracker.appspot.com",
-	messagingSenderId: "1026503451140"
+const firebaseConfig = {
+    apiKey: "AIzaSyBmk5NHsNEri6J1vISirSwsNQvIV3zaC5k",
+    authDomain: "twitch-tracker.firebaseapp.com",
+    databaseURL: "https://twitch-tracker.firebaseio.com",
+    projectId: "twitch-tracker",
+    storageBucket: "twitch-tracker.appspot.com",
+    messagingSenderId: "1026503451140"
 };
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 let statsRef = firebase.database().ref('/stats');
 
 exports.trackTwitch = functions.https.onRequest((req, resp) => {
-	let timestamp = new Date();
+    let timestamp = new Date();
 
-	// TODO: don't assume it's on page 2.  That's madness.  But the cursor seems to be evergreen
-	let options = {
-		url: 'https://api.twitch.tv/kraken/communities/top?limit=100&cursor=MTAw',
-		headers: {
-			'Accept': 'application/vnd.twitchtv.v5+json',
-			'Client-ID': 'qhqv7jyn0xk5tl4ogpmvxcf6n0l938',
-		}
-	};
+// TODO: deal with end of community list
+// Page through the API to find the programming community
+    return fetchAndCheck().then(function (programmingJson) {
+        let viewers = programmingJson['viewers'];
+        let channels = programmingJson['channels'];
 
-	request(options, function (error, response, body) {
-		if(!error) {
-			let communitiesList = JSON.parse(body)['communities'];
-			for(i in communitiesList) {
-				let community = communitiesList[i];
-				if(community['name'] === 'programming') {
-					let viewers = community['viewers'];
-					let channels = community['channels'];
+        resp.send(`${timestamp.toISOString()},${channels},${viewers}`);
 
-					resp.send(`${timestamp.toISOString()},${channels},${viewers}`);
-
-					// Write to Firebase Realtime Database
-					statsRef.push({
-					  timestamp: timestamp.toISOString(),
-						channels: channels,
-						viewers: viewers
-					});
-
-					break;
-				}
-			}
-			// TODO: deal with never finding it (page through results)
-		} else {
-			console.log("something went really wrong");
-			console.log(error);
-		}
-	});
+        // Write to Firebase Realtime Database
+        statsRef.push({
+            timestamp: timestamp.toISOString(),
+            channels: channels,
+            viewers: viewers
+        });
+    }).catch(function (err) {
+        resp.send(`bad stuff ${err}`);
+    });
 });
+
+function fetchAndCheck(cursor, pageDepth) {
+    // Deal with paging down too far
+    const maxPageDepth = 5;
+    if (pageDepth === undefined) {
+        // No value passed in?  Assume that I just started at page 0
+        pageDepth = 0;
+    }
+    if ( pageDepth > maxPageDepth ) {
+        throw `No programming community after ${pageDepth}.  Limit is ${maxPageDepth}`
+    }
+
+    // console.log("doing a request with cursor" + cursor);
+
+    let reqConfig = {
+        uri: 'https://api.twitch.tv/kraken/communities/top',
+        qs: {
+            limit: '100'
+        },
+        headers: {
+            'Client-ID': 'qhqv7jyn0xk5tl4ogpmvxcf6n0l938',
+            'Accept': 'application/vnd.twitchtv.v5+json'
+        },
+        json: true
+    };
+
+    // Only add a cursor if one has been passed in
+    if (cursor) {
+        reqConfig.qs.cursor = cursor;
+    }
+
+    return rp(reqConfig).then(function (resp) {
+        let programmingMaybe = _.find(resp.communities, {'name': 'programming'});
+        if (programmingMaybe === undefined) {
+            return fetchAndCheck(resp._cursor, pageDepth + 1);
+        } else {
+            return programmingMaybe;
+        }
+    });
+}
